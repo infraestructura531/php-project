42ac397eedfd66a5e72e1030cfe5161c08b9db9f
miavalentinaramos <mia.ramos@telam.com.ar>
2022-04-21 11:12:21 -0300

    herencia

--- src/public/15-herencia.php
+++ src/public/15-herencia.php
@@ -8,11 +8,63 @@ spl_autoload_register('mi_autocargador');
 $titulo = "herencia.php";
 include_once "../app/templates/header.php";
 ?>
+<p>en este caso crearemos una clase llamada padre con un funcion publica la cual hace un echo</p>
+<p>
+    <code>
+        <pre class="codigo">
 
+&lt;?php
+        class Padre
+        {
+            function __construct()
+            {
+            
+            }
+        public function funcion1(){
+            echo "Funcion 1 en la clase Padre";
+            }
+        }   
+?&gt;
+        </pre>
+    </code>
+</p>
+<p>  a su vez tambien crearemos una clase llamada Hija la cual podemos ver su constructor esta vacio y tampoco posee una funcion, pero "extiende" a clase padre por lo tanto posee la funcion display </p> 
+<p>
+    <code>
+        <pre class=codigo>
+
+&lt;?php
+        class Hija <b>extends</b> Padre
+        {
+            function __construct()
+            {
+        
+            }
+        }
+
+?&gt;
+        </pre>
+    </code>
+</p>
+
+<p>al instanciar hija, podemos notar que si llamamos a la funcion1 desde el $obj3 se mostrara por pantala el mensaje: '"Funcion 1 en la clase Padre"'</p>
+<p>
+    <code>
+        <pre class="codigo">
+        
+&lt;?php
+        $obj = new Hija();
+        $obj->funcion1();
+?&gt;
+        </pre>
+    </code>
+</p>
+<p>resultado:</p>
 <?php
 $obj = new Hija();
 $obj->funcion1();
 ?>
+
 <?php
 include_once "../app/templates/footer.php";
 ?>
