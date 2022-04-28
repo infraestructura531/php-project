9c1d0b019de17841563d3bdc0d5ac40a87260647
miavalentinaramos <mia.ramos@telam.com.ar>
2022-04-28 09:17:50 -0300

    index, nav, template

--- src/public/cursoPHP/03.MVC/index.php
+++ src/public/cursoPHP/03.MVC/index.php
@@ -1,7 +1,20 @@
 <?php
 #link https://www.youtube.com/watch?v=vgsLhG3VDCA
-#El INDEX: En el mosstraremos la salida de las vistas al usuario
+#El INDEX: En el mostraremos la salida de las vistas al usuario
 # y tambien a traves de el enviaremos las distintas acciones que el usuario envie al controlador
+
+#require() establece que el codigo del archivo invocado es requerido, es decir,
+#obligatorio para el funcionamiento del programa. por ello, si el archivo
+#especificado en la funcion require() no se encuentra saltaria un error
+#"PHP fatal error" y el programa php se detendra.
+
+#La version require_once() funcionan de la misma forma que su respectivo, 
+#salvo que, al utilizar la version _once, se impide la carga de un mismo archivo
+#mas de una vez.
+
+#si requerimos el mismo codigo mas de una vez corremos el riesgo de redeclaraciones
+#de variables, funciones o clases.
+
 require_once "controllers/controller.php";
 
 $mvc = new MvcController();
